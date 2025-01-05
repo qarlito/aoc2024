2bcedd08ac56efd9e8aa2b1019c3b342707a6ca1
Carl D'Halluin <carl.dhalluin@datadobi.com>
2025-01-05 23:02:50 +0100

    P24

--- P24_solution.py
+++ P24_solution.py
@@ -38,7 +38,7 @@ def op_xor(a, b):
 OPERATORS={'AND': op_and, 'OR': op_or, 'XOR': op_xor}
 
 known_states = dict()   # node to value          e.g. "x00" -> True
-dependencies = dict()   # node to creation rule  e.g. "z00" -> {"op":"AND", "inputs":{"x00", "y00"}, "needed": {"x00", "y00"}}
+dependencies = dict()   # node to creation rule  e.g. "z00" -> {"op":f_and, "inputs":{"x00", "y00"}}
 enablers = defaultdict(lambda:set())       # node to set of outputs e.g. "x00" -> {"z00"}
 
 input_part = 1
@@ -78,6 +78,94 @@ while len(known_states) > 0:
             output_state = dependencies[output]["op"](*(dependencies[output]["inputs"].values()))
             known_states[output] = output_state
 
-print(f'Part 1: {final_value}')
+print(f'Part 1: {final_value}\n\n')
 
 
+
+# Part 2
+
+orig_result = set()
+input_part = 1
+for line in INPUT2.splitlines():
+    if line == '':
+        assert input_part == 1
+        input_part = 2
+        continue
+    if input_part == 1:
+        # Ignore first part of input
+        continue
+    # Second part of input
+    formula, output = line.split(' -> ')
+    in1, op, in2 = formula.split(' ')
+    in1, in2 = sorted((in1, in2))
+    orig_result.add((in1, op, in2, output))
+
+
+####
+
+_REWIRINGS = {   'z05': 'tst', 'tst':'z05',
+                 'z11': 'sps', 'sps':'z11',
+                 'pmd': 'cgh', 'cgh':'pmd',
+                 'frt': 'z23', 'z23':'frt'
+             }
+
+REWIRINGS = dict()
+#REWIRINGS = _REWIRINGS
+
+rewired_result = list()
+for in1, op, in2, out in orig_result:
+    out = REWIRINGS.get(out, out)
+    rewired_result.append((in1, op, in2, out))
+
+print('-' * 40)
+print()
+
+carries = dict()
+carries[0] = 'wrs'
+
+for i in range(45):
+    and_xy = None
+    xor_xy = None
+    for in1, op, in2, out in rewired_result:
+        if {in1, in2} == {f'x{i:0>2}', f'y{i:0>2}'}:
+            if op == 'AND':
+                and_xy = out
+            if op == 'XOR':
+                xor_xy = out
+    if and_xy is None:
+        print('Problem andxy', i)
+    if xor_xy is None:
+        print('Problem xorxy', i)
+
+    print()
+    print(f'x{i:0>2} XOR y{i:0>2} -> {xor_xy}')
+
+    xor_xy_users = list()
+    for in1, op, in2, out in rewired_result:
+        if xor_xy in {in1, in2}:
+            xor_xy_users.append((in1, op, in2, out))
+            print(f'    {in1} {op} {in2} -> {out}')
+
+    print(f'x{i:0>2} AND y{i:0>2} -> {and_xy}')
+    and_xy_users = list()
+    for in1, op, in2, out in rewired_result:
+        if and_xy in {in1, in2}:
+            and_xy_users.append((in1, op, in2, out))
+            print(f'    {in1} {op} {in2} -> {out}')
+
+    if i!=0 and len(xor_xy_users) != 2:
+        print('error - wrong number of xorxy users')
+
+    if i==0 and len(and_xy_users) != 2:
+        print('error - wrong number of andxy users - expect 2')
+
+    if i!=0 and len(and_xy_users) != 1:
+        print('error - wrong number of andxy users - expect 1')
+
+    if i!=0:
+        # Want (x XOR y) XOR c -> z
+        found = False
+        for in1, op, in2, output in xor_xy_users:
+            if op == 'XOR' and output[0] != 'z':
+                print('error - want (x XOR y) XOR c -> z')
+        
